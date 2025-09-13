b3d10cdd949cfa025b5c2e66500e23e540361317
Karthick <karthicklingasamy6@gmail.com>
2025-09-13 11:12:56 +0530

    formated

--- application/views/hcpDashboard.php
+++ application/views/hcpDashboard.php
@@ -16,7 +16,7 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" />
     <!-- Image Cropper -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
-    
+
     <style>
         body {
             font-family: "Poppins", sans-serif;

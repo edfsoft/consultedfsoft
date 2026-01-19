d2cc35a48e9bdcf6973e23e0e39b60eda0b4169f
ArasukodiV <arasukodiedu@gmil.com>
2026-01-19 10:27:52 +0530

    token updated

--- application/controllers/AgoraController.php
+++ application/controllers/AgoraController.php
@@ -8,7 +8,7 @@ class AgoraController extends CI_Controller {
     $data['method']='videoCall';
     $data['app_id'] = 'f891d97665524065b626ea324f06942f';
     // Paste the temporary token you just copied here
-    $data['temp_token'] = '007eJxTYPgz6/bZiyeDq259nvnENjBoO4PZs/kLZjZMUnzrraeYFeCrwJBmYWmYYmluZmZqamRiYGaaZGZklppobGSSZmBmaWKUpmCfndkQyMgQuOcAMyMDBIL4nAwlqcUlukX5+bkMDABprCG9'; 
+    $data['temp_token'] = '007eJxTYPB+ITlNMN/W0z3t37UEhm9pYnPSZPZU+aWntUza/C4oTEOBIc3C0jDF0tzMzNTUyMTAzDTJzMgsNdHYyCTNwMzSxCjNbGduZkMgI8NEn63MjAwQCOJzMpSkFpfoFuXn5zIwAAA9rR8K'; 
     // The channel name must MATCH what you typed in the console
     $data['channel_name'] = 'test-room'; 
 

61c560a56ef53c7d5b7ae56de8f00ad6dae8d06d
Karthick <karthicklingasamy6@gmail.com>
2026-03-19 17:56:37 +0530

    mail config changed

--- application/config/email.php
+++ application/config/email.php
@@ -4,10 +4,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_port'] = 587; //  465
-// $config['smtp_user'] = 'erodediabetesfoundation@gmail.com';
-// $config['smtp_pass'] = 'aicbbfdzxzccqpfc';
-$config['smtp_user'] = 'santhoshamparentalcare@gmail.com';
-$config['smtp_pass'] = 'ruzn hytj fncv jkcn';
+$config['smtp_user'] = 'erodediabetesfoundation@gmail.com';
+$config['smtp_pass'] = 'aicbbfdzxzccqpfc';
 $config['mailtype'] = 'html';
 $config['charset'] = 'utf-8';
 $config['wordwrap'] = TRUE;
